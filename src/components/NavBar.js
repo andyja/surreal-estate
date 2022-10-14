e5199d264cef68b11c6b468e3d18e4942ee92e30
Andy Jackson <andypauljackson@gmail.com>
2022-10-14 10:07:31 +0100

    some styling added to navbar logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "../styles/navbar.css";
 
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <img className="s-e-logo" src="/logos/s-e-logo.png" alt="company logo" />
+      <img className="logo" src="/logos/logo.png" alt="company logo" />
     </div>
   );
 };

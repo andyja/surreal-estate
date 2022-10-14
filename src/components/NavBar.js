efd7868e3e8da5d1e345e213d48830e5ea78d74f
Andy Jackson <andypauljackson@gmail.com>
2022-10-14 10:54:44 +0100

    NavBar links added, no router added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,18 @@ const NavBar = () => {
   return (
     <div className="NavBar">
       <img className="logo" src="/logos/logo.png" alt="company logo" />
+      <ul className="navbar-links">
+        <li className="navbar-links-item">
+          <Link className="item" to="/">
+            View Properties
+          </Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link className="item" to="add-property">
+            Add a Property
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };

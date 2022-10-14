6b1b5bc95082f5fafbfd22d5811107567a1ff58d
Andy Jackson <andypauljackson@gmail.com>
2022-10-14 09:43:24 +0100

    se logo renders through navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,10 @@
 import React from "react";
 
 const NavBar = () => {
-
-    return
-
-
-
-<div className="NavBar">
-    
-
-</div>
-
-}
+  return (
+    <div className="NavBar">
+      <img className="s-e-logo" src="/logos/s-e-logo.png" alt="company logo" />
+    </div>
+  );
+};
 export default NavBar;

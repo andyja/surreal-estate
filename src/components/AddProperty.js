ff90f0fdf5423ac8f11512064f563c271e53c8a8
Andy Jackson <andypauljackson@gmail.com>
2022-11-25 09:59:07 +0000

    addproperty in progress'

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/add-property.css";
 
 const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+    },
+  };
+
+  const [fields, setFields] = useState(initalState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({...fields, [event.target.name]: event.target.value });
+  };
+
   return (
-    <div>
+    <div className="add-property">
+      <form onSubmit={handleAddProperty}>
+        <input
+          id="title"
+          name="title"
+          placeholder="title"
+          type="text"
+          value={fields.title}
+          onChange={handleFieldChange}
+        />
+        <button type="submit">Add</button>
+      </form>
       <p>
         This is the add property page Enter property information and click
         submit to add a porperty to the database

c5cc96e6d61d72494ce87f2b9ffe86ff29ed899e
Andy Jackson <andypauljackson@gmail.com>
2022-12-09 11:02:15 +0000

    email added to add property

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -8,15 +8,14 @@ const AddProperty = () => {
     },
   };
 
-  const [fields, setFields] = useState(initalState.fields);
+  const [fields, setFields] = useState(initialState.fields);
 
   const handleAddProperty = (event) => {
     event.preventDefault();
-    console.log(fields);
   };
 
   const handleFieldChange = (event) => {
-    setFields({...fields, [event.target.name]: event.target.value });
+    setFields({ ...fields, [event.target.name]: event.target.value });
   };
 
   return (
@@ -30,7 +29,84 @@ const AddProperty = () => {
           value={fields.title}
           onChange={handleFieldChange}
         />
-        <button type="submit">Add</button>
+        <div className="form-field">
+          <label htmlFor="city">
+            City
+            <select
+              id="city"
+              name="city"
+              value={fields.city}
+              onChange={handleFieldChange}
+            >
+              <option value="Leeds">Leeds</option>
+              <option value="Manchester">Manchester</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Liverpool">Liverpool</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="type">
+            House Type
+            <select
+              id="type"
+              name="type"
+              value={fields.type}
+              onChange={handleFieldChange}
+            >
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Bungalow">Bungalow</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="bedrooms">
+            Bedrooms
+            <input
+              id="bedrooms"
+              name="bedrooms"
+              placeholder="Bedrooms"
+              type="number"
+              value={fields.bedrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="price">
+            Price
+            <input
+              id="price"
+              name="price"
+              placeholder="100000"
+              type="number"
+              value={fields.price}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="email">
+            email
+            <input
+              id="email"
+              name="email"
+              placeholder="example@gmail.com"
+              type="text"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="button-container">
+          <div className="button-form-field">
+            <button type="submit">Add</button>
+          </div>
+        </div>
       </form>
       <p>
         This is the add property page Enter property information and click
